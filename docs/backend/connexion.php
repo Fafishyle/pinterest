d71c57614aca1a479506509064aa1131cd71b0af
Fina <finaritralft@gmail.com>
2025-02-02 00:31:35 +0100

    controll allow origin accepte dans inscription et connexion.php

--- docs/backend/connexion.php
+++ docs/backend/connexion.php
@@ -1,5 +1,9 @@
 <?php
     header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+
     // Je me connecte à la base de données.
 
     $con = mysqli_connect('localhost', 'root', '') or die (mysqli_error());
--- docs/backend/inscription.php
+++ docs/backend/inscription.php
@@ -1,5 +1,9 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 // Récupérer l'URL de la base de données depuis les variables d'environnement Heroku
 $DATABASE_URL = getenv('DATABASE_URL');
